2b8038db0386fb80c6754ce67007a2a10b3a34e6
EnigmaCurry <ryan@enigmacurry.com>
2021-01-08 10:01:35 -0800

    ubuntu_installer.sh add more nginx config

--- contrib/ubuntu_installer.sh
+++ contrib/ubuntu_installer.sh
@@ -227,7 +227,12 @@ server {
         proxy_http_version 1.1;
         proxy_set_header Upgrade @@@http_upgrade;
         proxy_set_header Connection "Upgrade";
+        proxy_set_header X-Real-IP @@@remote_addr;
+        proxy_set_header X-Forwarded-For @@@proxy_add_x_forwarded_for;
         proxy_set_header Host @@@host;
+        proxy_connect_timeout   24h;
+        proxy_send_timeout      24h;
+        proxy_read_timeout      24h;
     }
     ssl_certificate /etc/letsencrypt/live/${DOMAIN}/fullchain.pem;
     ssl_certificate_key /etc/letsencrypt/live/${DOMAIN}/privkey.pem;
